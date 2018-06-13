2cf2f15e5d8d703c0e7aaae8614278755440c1b4
Adam Snape <adam@snape.xyz>
2018-06-13 20:26:45 +0100

    add event listeners to all pixel elements

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,13 +1,22 @@
-//globals
 const screen = document.querySelector('#screen');
 
 //create pixel grid
 function createGrid(size) {
-    for(i= 0; i < size*size; i++) {
+    for (i = 0; i < size * size; i++) {
         let pixel = document.createElement('div');
         pixel.className = 'pixel';
         screen.appendChild(pixel);
     }
 }
-
 createGrid(16);
+
+//add hover event listeners
+const pixels = Array.from(document.querySelectorAll(".pixel"));
+function onHover() {
+    for (i = 0; i < pixels.length; i++) {
+        pixels[i].addEventListener('mouseover', function (e) {
+            this.style.opacity = 0.1;
+        });
+    }
+}
+onHover();
