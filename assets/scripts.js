fb31a7ad4f64d47e62285e4284adaedf991a1a47
Adam Snape <adam@snape.xyz>
2018-06-14 10:30:52 +0100

    pixels get darker with each hover

--- assets/scripts.js
+++ assets/scripts.js
@@ -3,7 +3,7 @@ const screen = document.querySelector('#screen');
 //create pixel grid
 function createGrid(size) {
     document.styleSheets[1].cssRules[3].style["grid-template-columns"] = "repeat(" + size + ", 1fr)"
-    document.styleSheets[1].cssRules[3].style["grid-template-rows"] =  "repeat(" + size + ", 1fr)"
+    document.styleSheets[1].cssRules[3].style["grid-template-rows"] = "repeat(" + size + ", 1fr)"
     for (i = 0; i < size * size; i++) {
         let pixel = document.createElement('div');
         pixel.className = 'pixel';
@@ -18,7 +18,7 @@ function onHover() {
     let pixels = Array.from(document.querySelectorAll(".pixel"));
     for (i = 0; i < pixels.length; i++) {
         pixels[i].addEventListener('mouseover', function (e) {
-            this.style.opacity = 0.1;
+            (this.style.opacity) ? this.style.opacity = parseFloat(this.style.opacity) + 0.1 : this.style.opacity = 0.1;
         });
     }
 }
@@ -26,11 +26,14 @@ onHover();
 
 
 // clear board on click
-document.querySelector("#left-knob").addEventListener('click', function(e) {
-    while (screen.hasChildNodes()) {
-        screen.removeChild(screen.lastChild);
-    }
+document.querySelector("#left-knob").addEventListener('click', function (e) {
     let newsize = Number(window.prompt('please provide a new grid size', 16));
-    createGrid(newsize);
-    onHover();
+
+    if(newsize) {
+        while (screen.hasChildNodes()) {
+            screen.removeChild(screen.lastChild);
+        }
+        createGrid(newsize);
+        onHover();
+    }
 })
