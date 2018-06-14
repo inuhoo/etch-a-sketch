13bdc67ae6493d5dab76d8495791dc1f432ac57a
Adam Snape <adam@snape.xyz>
2018-06-14 07:55:42 +0100

    left-knob clears board and sets new size

--- assets/scripts.js
+++ assets/scripts.js
@@ -2,6 +2,8 @@ const screen = document.querySelector('#screen');
 
 //create pixel grid
 function createGrid(size) {
+    document.styleSheets[1].cssRules[3].style["grid-template-columns"] = "repeat(" + size + ", 1fr)"
+    document.styleSheets[1].cssRules[3].style["grid-template-rows"] =  "repeat(" + size + ", 1fr)"
     for (i = 0; i < size * size; i++) {
         let pixel = document.createElement('div');
         pixel.className = 'pixel';
@@ -11,8 +13,9 @@ function createGrid(size) {
 createGrid(16);
 
 //add hover event listeners
-const pixels = Array.from(document.querySelectorAll(".pixel"));
+
 function onHover() {
+    let pixels = Array.from(document.querySelectorAll(".pixel"));
     for (i = 0; i < pixels.length; i++) {
         pixels[i].addEventListener('mouseover', function (e) {
             this.style.opacity = 0.1;
@@ -20,3 +23,14 @@ function onHover() {
     }
 }
 onHover();
+
+
+// clear board on click
+document.querySelector("#left-knob").addEventListener('click', function(e) {
+    while (screen.hasChildNodes()) {
+        screen.removeChild(screen.lastChild);
+    }
+    let newsize = Number(window.prompt('please provide a new grid size', 16));
+    createGrid(newsize);
+    onHover();
+})
